fa096a7f6b53fed0b9be4da69d8e8d1ad678723b
raikaat <raikaat@gmail.com>
2018-02-21 23:21:54 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr = equation.split(' ');
+  var result = [];
+
+  var a = +arr[0];
+  var b = arr[3] === '-' ? -+arr[4] : +arr[4];
+  var c = arr[7] === '-' ? -+arr[8] : +arr[8];
+  var discriminant = Math.pow(b,2)-4*a*c;
+  result.push(Math.round((-b-Math.sqrt(discriminant))/(2*a)));
+  result.push(Math.round((-b+Math.sqrt(discriminant))/(2*a)));
+  return result.sort(function(a,b){
+    return a-b;
+  });
 }
